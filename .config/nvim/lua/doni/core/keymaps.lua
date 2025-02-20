a8eef8724a003c5764800363b310fe64675dd083
Alexandru Doni <donialexandru@gmail.com>
2025-02-20 15:31:10 +0200

    almost done

--- .config/nvim/lua/doni/core/keymaps.lua
+++ .config/nvim/lua/doni/core/keymaps.lua
@@ -4,7 +4,29 @@ local keymap = vim.keymap
 
 keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
 
-keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+keymap.set("x", "<leader>p", [["_dP]])
+
+keymap.set("n", "<leader>y", [["+y]])
+keymap.set("v", "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+keymap.set("n", " Q", "<nop>")
+
+keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+keymap.set("n", "<leader>", "<cmd>!chmod +x %<CR>", { silent = true })
